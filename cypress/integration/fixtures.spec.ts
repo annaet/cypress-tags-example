ef2cd2b7e75e30497f4cd11afc4f32bf5d778e9d
Anna Thomas <anna.thomas@infosum.com>
2021-02-23 15:52:54 +0000

    Add new test examples from main repo and update version

---
+++ cypress/integration/fixtures.spec.ts
@@ -0,0 +1,17 @@
+import fixture from '../fixtures/example.json';
+
+describe('Uses fixtures', () => {
+  beforeEach(() => {
+    cy.fixture('example').as('fixture');
+  });
+
+  it('should load fixture', function () {
+    expect(this.fixture.foo).to.equal('bar');
+    expect(fixture.foo).to.equal('bar');
+  });
+
+  it(['wip'], 'should load fixture when filtered', function () {
+    expect(this.fixture.foo).to.equal('bar');
+    expect(fixture.foo).to.equal('bar');
+  });
+});
---
+++ cypress/integration/for_each.spec.ts
@@ -0,0 +1,21 @@
+describe('forEach', 'Uses a forEach', () => {
+  let keys = [1, 2, 3];
+
+  keys.forEach((key, i) => {
+    it('forEach', `should get key ${key}`, () => {
+      expect(key).to.equal(keys[i]);
+    });
+  });
+
+  const testObject = {
+    foo: 'bar',
+    fizz: 'buzz',
+  };
+  let objectKeys = Object.keys(testObject);
+
+  objectKeys.forEach((key) => {
+    it(`should get key ${key}`, () => {
+      expect(objectKeys).to.include(key);
+    });
+  });
+});
